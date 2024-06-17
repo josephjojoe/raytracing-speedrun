f5f6423eada961aab25db6d1555dbb85bcab5312
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-16 19:55:06 -0700

    Okay *now* image generation with rays works again haha

--- main.cc
+++ main.cc
@@ -1,19 +1,47 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 #include <iostream>
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
+
 int main() {
+    auto aspect = 16.0 / 9.0;
+    int width = 400;
 
-    int width = 256;
-    int height = 256;
-    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+    int height = int(width / aspect);
+    height = (height < 1) ? 1 : height;
 
-    for (int j = 0; j < width; j++) {
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(width)/height);
+    auto camera_center = point3(0, 0, 0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / width;
+    auto pixel_delta_v = viewport_v / height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    std::cout << "P3\n" << width << " " << height << "\n255\n";
+
+    for (int j = 0; j < height; j++) {
         std::clog << "\rScanlines remaining: " << (height - j) << " " << std::flush;
-        for (int i = 0; i < height; i++) {
-            auto pixel_color = color(double(i)/(width-1), double(j)/(height-1), 0);
+        for (int i = 0; i < width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
-    std::clog << "\rDone.                YAY\n";
+
+    std::clog << "\rDone.      \n";
 }
