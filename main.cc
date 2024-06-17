d30feddc25a0deac09e3ef2a7b43cb00aba6bc5d
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-16 20:01:46 -0700

    Generated gradient with sphere and associated code

--- main.cc
+++ main.cc
@@ -3,7 +3,20 @@
 #include "vec3.h"
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -33,7 +46,7 @@ int main() {
     std::cout << "P3\n" << width << " " << height << "\n255\n";
 
     for (int j = 0; j < height; j++) {
-        std::clog << "\rScanlines remaining: " << (height - j) << " " << std::flush;
+        std::clog << "\rScanlines remaining: " << (height - j) << ' ' << std::flush;
         for (int i = 0; i < width; i++) {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
@@ -43,5 +56,5 @@ int main() {
         }
     }
 
-    std::clog << "\rDone.      \n";
+    std::clog << "\rDone.                 \n";
 }
