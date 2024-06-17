0f0bab10ee82056ba2bbe1aaa3e0f8d8d5cb10eb
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-16 19:17:09 -0700

    Created first main.cc file to generate images

---
+++ main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    int width = 256;
+    int height = 256;
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+
+    for (int j = 0; j < width; j++) {
+        for (int i = 0; i < height; i++) {
+            auto r = double(i) / (width-1);
+            auto g = double(j) / (height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
