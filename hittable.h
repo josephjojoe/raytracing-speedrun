df1b2d6d544abc46970b8f30dd4cf5c3ddeef2d8
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-16 20:41:46 -0700

    Added hit using interval

--- hittable.h
+++ hittable.h
@@ -7,6 +7,7 @@ class hit_record {
         vec3 normal;
         double t;
         bool front_face;
+        virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
