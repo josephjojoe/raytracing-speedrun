30904e827c365ef44fb4040f5f6e5feeac4be550
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-17 00:30:15 -0700

    Set new samples per pixel paramater

--- main.cc
+++ main.cc
@@ -4,8 +4,6 @@
 #include "hittable_list.h"
 #include "sphere.h"
 
-
-
 int main() {
     hittable_list world;
 
@@ -16,6 +14,7 @@ int main() {
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width  = 400;
+    cam.samples_per_pixel = 100;
 
     cam.render(world);
 }
