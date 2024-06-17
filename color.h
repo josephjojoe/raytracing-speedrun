002694160ab115751c82816bf24e930b826bcb01
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-16 19:35:15 -0700

    Typed out color.h which was a lot shorter and hence far more tolerable

---
+++ color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // The book calls this a translation from [0,1] to [0, 255].
+    // I feel like that's more of a transformation.
+    // Enlargement, maybe? Who knows.
+
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    out << rbyte << " " << gbyte << " " << bbyte << "\n";
+}
+
+#endif
