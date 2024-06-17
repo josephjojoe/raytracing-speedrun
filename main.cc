4117400b4b92d63023e0452e497557dc90396082
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-16 19:20:53 -0700

    Added progress indicator

--- main.cc
+++ main.cc
@@ -7,6 +7,7 @@ int main() {
     std::cout << "P3\n" << width << ' ' << height << "\n255\n";
 
     for (int j = 0; j < width; j++) {
+        std::clog << "\rScanlines remaining: " << (height - j) << " " << std::flush;
         for (int i = 0; i < height; i++) {
             auto r = double(i) / (width-1);
             auto g = double(j) / (height-1);
@@ -19,4 +20,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone :D\n";
 }
