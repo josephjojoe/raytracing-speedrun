f729e651c4e2d83d4d7bd038c40ed8da4ad0f561
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-06-18 19:00:28 -0700

    Applied gamma correction to color.h

--- color.h
+++ color.h
@@ -6,10 +6,21 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return sqrt(linear_component);
+    }
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // The book calls this a translation from [0,1] to [0, 255].
     // I feel like that's more of a transformation.
